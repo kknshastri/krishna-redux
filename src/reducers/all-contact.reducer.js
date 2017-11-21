48aab3a463a06c7ac5a825e2f1bb93628e799e9b
kknshastri <kknshastri@gmail.com>
2017-11-21 18:21:35 +0530

    Update all-contact.reducer.js

--- src/reducers/all-contact.reducer.js
+++ src/reducers/all-contact.reducer.js
@@ -4,8 +4,13 @@ export default function(state = mockData.allContacts, action) {
 	switch(action.type) {
         case 'CONTACT_CREATE': {
 			// return [Object.assign({}, action.payload, {'id': state.length}), ...state];
-			return [Object.assign({}, action.payload,
-				{'id': (Math.max(...state.map(c => c.id))) + 1}), ...state];
+			if(!!state.length) {
+				return [
+					Object.assign({}, action.payload, {'id': (Math.max(...state.map(c => c.id))) + 1}),
+					...state];
+			} else {
+				return [Object.assign({}, action.payload, {'id':1})];
+			}
 		}
         case 'CONTACT_DELETE': {return state.filter(c => c.id !== action.payload.id);}
 		case 'CONTACT_UPDATE': {
